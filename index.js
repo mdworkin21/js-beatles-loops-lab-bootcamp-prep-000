487878127826a33fdac6adced2a92375293d8861
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-27 19:45:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function johnLennonFacts (array){
     "He was a choir boy and boy scout",
     "He hated the sound of his own voice"
   ];
-  while(i < array.length + 1){
+  while(i < array[i] + 1){
       exclaim.push(array[i] + "!!!")
       i++;
     }
