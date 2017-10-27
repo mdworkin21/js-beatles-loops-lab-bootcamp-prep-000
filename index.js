100e50846d9b78f7032b5909a39a53deb28a233b
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-27 19:26:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var facts = [
   "He hated the sound of his own voice"
   ];
 
-function johnLennonFacts (){
+function johnLennonFacts (array){
   while(i < array.length){
       exclaim.push(array[i] + "!!!")
       i++;
