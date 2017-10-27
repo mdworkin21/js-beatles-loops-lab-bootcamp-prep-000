76aea88bdb69ef8e2257d8261aad020c25f4c4ac
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-27 19:37:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,14 +17,15 @@ function theBeatlesPlay(array1, array2){
 
 var i = 0
 var exclaim = []
-var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
 
-function johnLennonFacts (number){
+
+function johnLennonFacts (array){
+  var facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+  ];
   while(i < array.length){
       exclaim.push(array[i] + "!!!")
       i++;
